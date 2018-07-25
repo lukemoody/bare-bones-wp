26a4e89f729ad26455b45609216e9c27f0833096
Luke Moody <lukemoody@Developer.local>
2018-07-25 15:13:30 +0100

    gulp-plumber added and setup.

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,7 @@ var gulp           = require('gulp');
     sass           = require('gulp-sass');
     mainBowerFiles = require('main-bower-files');
     watch          = require('gulp-watch');
+    plumber        = require('gulp-plumber');
 
 /*
  * gulp-sass
@@ -18,6 +19,7 @@ var gulp           = require('gulp');
 
 gulp.task('sass', function(){
   return gulp.src('inc/scss/**/*.scss')
+    .pipe(plumber())
     .pipe(sass())
     .pipe(gulp.dest('inc/css/src'))
     .pipe(prefix('last 2 versions'))
@@ -51,6 +53,7 @@ gulp.task('concat', function () {
 
   gulp.src(['inc/js/libs/*.js' , 'inc/js/*.js'])
     .pipe(concat('main.min.js'))
+    .pipe(plumber())
     .pipe(uglify())
     .pipe(gulp.dest('dist')); // dist/main.min.js
 });
@@ -61,7 +64,7 @@ gulp.task('concat', function () {
 
 gulp.task('browserSync', function() {
   browserSync.init({
-    proxy: "http://local-bare-bones-wp:8888/", // "http://local.dev", // Replace this with your local dev environment to work with BrowserSync local host
+    proxy: "http://local.dev", // Replace this with your local dev environment to work with BrowserSync local host
     watchTask: true,
   })
 });
